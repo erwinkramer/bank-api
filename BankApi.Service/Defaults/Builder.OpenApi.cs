901bd9aca04efe83ed689b813e1f40eb7a51e47a
Erwin Kramer <r3verse@gmail.com>
2024-11-24 18:14:05 +0100

    remove redundant apiExplorer call

--- BankApi.Service/Defaults/Builder.OpenApi.cs
+++ BankApi.Service/Defaults/Builder.OpenApi.cs
@@ -4,7 +4,6 @@ static partial class ApiBuilder
 {
     public static IServiceCollection AddOpenApiServices(this IServiceCollection services)
     {
-        services.AddEndpointsApiExplorer();
         services.AddOpenApi(GlobalConfiguration.ApiDocument!.Info.Version, options =>
         {
             options.AddDocumentTransformer<TransformerDocInfo>();
