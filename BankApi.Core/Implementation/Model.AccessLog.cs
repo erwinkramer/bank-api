3731d53b9654651d119c4dbcb0d490dc8ffde12d
Erwin Kramer <r3verse@gmail.com>
2024-12-13 16:07:58 +0100

    cspell

--- BankApi.Core/Implementation/Model.AccessLog.cs
+++ BankApi.Core/Implementation/Model.AccessLog.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 public class AccessLogModel
 {
     [RestrictedData]
-    [Description("User Id of the requestee.")]
+    [Description("User Id of the requestor.")]
     public string? UserId { get; set; }
 
     [Description("Authentication type during access.")]
