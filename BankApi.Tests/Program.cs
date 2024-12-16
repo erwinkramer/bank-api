28492ac13377864f272aa55b975fe221cc2eaa09
Erwin Kramer <r3verse@gmail.com>
2024-12-17 00:57:28 +0100

    fix tests

--- BankApi.Tests/Program.cs
+++ BankApi.Tests/Program.cs
@@ -3,6 +3,7 @@
 public class Test
 {
     private static BankDb? databaseContext;
+    private static string bankId ="29e26195-cf57-417d-ac1b-998398e2dc88";
 
     [Before(Class)]
     public static Task CreateContext()
@@ -14,27 +15,26 @@ public class Test
     [Test]
     public async Task CreateBankReturnsCreated()
     {
-        var result = await BankOperation.CreateBank(new BankModel()
+        var response = await BankOperation.CreateBank(new BankModel()
         {
             BankTier = BankTier.A,
-            Id = Guid.NewGuid(),
+            Id = Guid.Parse(bankId),
             IsCompliant = false
         }, databaseContext!);
 
-        await Assert.That(result).IsTypeOf<Created<BankModel>>();
+        await Assert.That(response.Result).IsTypeOf<Created<BankModel>>();
     }
 
     [Test, DependsOn(nameof(CreateBankReturnsCreated))]
     public async Task UpdateBankReturnsNoContent()
     {
-        var bankId = Guid.NewGuid();
-        var result = await BankOperation.UpdateBank(bankId, new BankModel()
+        var response = await BankOperation.UpdateBank(Guid.Parse(bankId), new BankModel()
         {
             BankTier = BankTier.A,
-            Id = bankId,
+            Id = Guid.Parse(bankId),
             IsCompliant = true
         }, databaseContext!);
 
-        await Assert.That(result).IsTypeOf<Results<NoContent, NotFound>>();
+        await Assert.That(response.Result).IsTypeOf<NoContent>();
     }
 }
