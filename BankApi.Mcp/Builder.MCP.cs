64542f41d8be2b5bf0eeb572d987505d3a190b7a
Erwin <erwinkramer@hotmail.com>
2025-11-27 19:10:05 +0100

    remove OAuthGrantTypeAuthenticator for now

--- BankApi.Mcp/Builder.MCP.cs
+++ BankApi.Mcp/Builder.MCP.cs
@@ -19,18 +19,6 @@ public static partial class ApiBuilder
         var serverInfoBuilder = McpServerInfoBuilder.ForOpenApi("bankapi")
             .FromFile(openApiPath)
             .AddDefaultHeader("User-Agent", "QuickMCP Client")
-            .AddAuthentication(
-                new OAuthGrantTypeAuthenticator(
-                    tokenUrl: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/authorize",
-                    grantType: "code",
-                    clientId: "b6997777-3799-4c55-b78a-4ce96e3d959c",
-                    grantParameters: new Dictionary<string, string>
-                    {
-                        { "code_challenge","somevalue" },
-                        { "code_challenge_method", "plain" } // S256 or plain
-                    },
-                    scope: "b6997777-3799-4c55-b78a-4ce96e3d959c/.default")
-            )
             .AddAuthentication(new ApiKeyAuthenticator("Lifetime Subscription", "Ocp-Apim-Subscription-Key", ApiKeyAuthenticator.ApiKeyLocation.Header));
 
         // Build server info
