89dd052a9187697b7b056fa4e11f554f98b1a011
Erwin <erwinkramer@hotmail.com>
2025-12-14 11:11:53 +0100

    mcp v6

--- BankApi.Mcp/Builder.MCP.cs
+++ BankApi.Mcp/Builder.MCP.cs
@@ -1,4 +1,5 @@
 using MCPify.Core;
+using MCPify.Core.Auth;
 using MCPify.Core.Auth.OAuth;
 using MCPify.Hosting;
 
@@ -21,15 +22,15 @@ public static partial class ApiBuilder
             options.ExternalApis.Add(new()
             {
                 SwaggerFilePath = openApiPath,
-               
                 ApiBaseUrl = $"https://localhost:5201/{apiVersion}",
                 ToolPrefix = "bankapi",
-                Authentication = new OAuthAuthorizationCodeAuthentication(
+                AuthenticationFactory = sp => new OAuthAuthorizationCodeAuthentication(
                     clientId: "b6997777-3799-4c55-b78a-4ce96e3d959c",
                     authorizationEndpoint: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/authorize",
                     tokenEndpoint: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/token",
                     scope: "b6997777-3799-4c55-b78a-4ce96e3d959c/.default",
-                    tokenStore: new FileTokenStore("token.json") // Persist token to disk
+                    secureTokenStore: sp.GetRequiredService<ISecureTokenStore>(),
+                    mcpContextAccessor: sp.GetRequiredService<IMcpContextAccessor>()
                 )
             });
         });
