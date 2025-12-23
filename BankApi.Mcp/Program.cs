2495d617e57952505ab18370c59a1f124dcd9433
Erwin <erwinkramer@hotmail.com>
2025-12-23 12:39:25 +0100

    fixes

--- BankApi.Mcp/Program.cs
+++ BankApi.Mcp/Program.cs
@@ -6,17 +6,13 @@ await builder.Services.AddMCPService("v1");
 
 var app = builder.Build();
 
-// Enable MCPify's context accessor middleware
-app.UseMcpifyContext();
-//app.UseMcpifyOAuth();
-
+app.UseMcpifyContext(); // Enable MCPify's context accessor middleware
 app.MapAuthCallback("/auth/callback");
 
 // Register MCP Tools (Must be called after endpoints are mapped but before Run)
 var registrar = app.Services.GetRequiredService<McpifyServiceRegistrar>();
 await registrar.RegisterToolsAsync(((IEndpointRouteBuilder)app).DataSources);
 
-// Map the MCP Endpoint (for HTTP transport) and OAuth metadata endpoint
-app.MapMcpifyEndpoint(); 
+app.MapMcpifyEndpoint(); // Map the MCP Endpoint (for HTTP transport) and OAuth metadata endpoint
 
 app.Run();
