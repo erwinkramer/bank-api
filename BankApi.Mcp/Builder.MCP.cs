829f9c6ead958141adb94737f134d270dc8c7ddb
Erwin <erwinkramer@hotmail.com>
2026-01-17 00:39:20 +0100

    mcp OAuthConfigurations preparation

--- BankApi.Mcp/Builder.MCP.cs
+++ BankApi.Mcp/Builder.MCP.cs
@@ -18,19 +18,19 @@ public static partial class ApiBuilder
         var openApiPath = Path.Combine(AppContext.BaseDirectory, $"openapi_{apiVersion}.json");
 
         services.AddScoped(sp =>
-        {
-            return new OAuthAuthorizationCodeAuthentication(
-                clientId: "b6997777-3799-4c55-b78a-4ce96e3d959c",
-                clientSecret: "fQB8Q~GkKsBaQFKnrTLEGXpRHWejyASJB6ZMGba~", // we need this because when running in Claude and authenticating via our MCP server, it's not a SPA.
-                authorizationEndpoint: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/authorize",
-                tokenEndpoint: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/token",
-                scope: "b6997777-3799-4c55-b78a-4ce96e3d959c/.default",
-                secureTokenStore: sp.GetRequiredService<ISecureTokenStore>(),
-                mcpContextAccessor: sp.GetRequiredService<IMcpContextAccessor>(),
-                redirectUri: "https://bankapi-mcp-001-ctcahwhschgrdqb4.westeurope-01.azurewebsites.net/auth/callback",
-                usePkce: true
-            );
-        });
+            {
+                return new OAuthAuthorizationCodeAuthentication(
+                    clientId: "b6997777-3799-4c55-b78a-4ce96e3d959c",
+                    clientSecret: "fQB8Q~GkKsBaQFKnrTLEGXpRHWejyASJB6ZMGba~", // we need this because when running in Claude and authenticating via our MCP server, it's not a SPA.
+                    authorizationEndpoint: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/authorize",
+                    tokenEndpoint: "https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/oauth2/v2.0/token",
+                    scope: "b6997777-3799-4c55-b78a-4ce96e3d959c/.default",
+                    secureTokenStore: sp.GetRequiredService<ISecureTokenStore>(),
+                    mcpContextAccessor: sp.GetRequiredService<IMcpContextAccessor>(),
+                    redirectUri: "https://bankapi-mcp-001-ctcahwhschgrdqb4.westeurope-01.azurewebsites.net/auth/callback",
+                    usePkce: true
+                );
+            });
 
         services.AddScoped(sp =>
         {
@@ -44,6 +44,10 @@ public static partial class ApiBuilder
         {
             options.ResourceUrlOverride = "https://bankapi-mcp-001-ctcahwhschgrdqb4.westeurope-01.azurewebsites.net";
             options.Transport = McpTransportType.Http;
+            //options.OAuthConfigurations.Add(new()
+            //{
+            //    AuthorizationServers = ["https://login.microsoftonline.com/b81eb003-1c5c-45fd-848f-90d9d3f8d016/v2.0"]
+            //});
             options.ExternalApis.Add(new()
             {
                 OpenApiFilePath = openApiPath,
