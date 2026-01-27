15f0d6a2e1d1b6d7153da6c2462c9d8176d09143
Erwin <erwinkramer@hotmail.com>
2026-01-27 22:10:21 +0100

    AddAuthentication

--- BankApi.Mcp/Program.cs
+++ BankApi.Mcp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using MCPify.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 await builder.Services.AddMCPService(builder.Configuration["ApiBaseUrl"]!, "v1", builder.Configuration["McpServerBaseUrl"]!);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            // In development, we accept any valid token without validating the signature (via the authority)
+            // this is to simplify local development with self-signed tokens
+            options.TokenValidationParameters.SignatureValidator = (token, _) => new JsonWebToken(token);
+        }
+    });
+
 builder.Services.AddAuthorization(options =>
     options.DefaultPolicy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
         .RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
         .Build());
 
 var app = builder.Build();
