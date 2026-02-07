dc6d77f7cc6ee0258017a0f334362d08f4a03cec
Erwin <erwinkramer@hotmail.com>
2026-02-07 18:59:21 +0100

    simplify

--- BankApi.Core/Defaults/Builder.OpenApi.cs
+++ BankApi.Core/Defaults/Builder.OpenApi.cs
@@ -9,7 +9,7 @@ public static partial class ApiBuilder
         {
             // Purposely set the spec to a version that is supported by Azure APIM, 
             // the 'Specs.Generated' output files still use the latest spec versions.
-            options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0; 
+            options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
 
             options.AddDocumentTransformer<TransformerDocInfo>();
             options.AddDocumentTransformer<TransformerComponentSchemas>();
@@ -40,10 +40,10 @@ public static partial class ApiBuilder
                 options.ClientId = GlobalConfiguration.ApiSettings!.EntraId.ClientId;
                 options.ClientSecret = "fQB8Q~GkKsBaQFKnrTLEGXpRHWejyASJB6ZMGba~";
                 options.CredentialsLocation = CredentialsLocation.Body;
-                options.SelectedScopes = new List<string>
-                {
+                options.SelectedScopes =
+                [
                     $"{GlobalConfiguration.ApiSettings!.EntraId.ClientId}/.default"
-                };
+                ];
             });
             options.AddAuthorizationCodeFlow("OAuth2", options =>
             {
@@ -52,12 +52,13 @@ public static partial class ApiBuilder
                 options.RefreshUrl = $"https://login.microsoftonline.com/{GlobalConfiguration.ApiSettings!.EntraId.TenantId}/oauth2/v2.0/token";
                 options.ClientId = GlobalConfiguration.ApiSettings!.EntraId.ClientId;
                 options.CredentialsLocation = CredentialsLocation.Body;
-                options.SelectedScopes = new List<string>
-                {
+                options.SelectedScopes =
+                [
                     $"{GlobalConfiguration.ApiSettings!.EntraId.ClientId}/.default"
-                };
+                ];
                 options.Pkce = Pkce.Sha256;
             });
+            options.AddDefaultScopes("OAuth2", [$"{GlobalConfiguration.ApiSettings!.EntraId.ClientId}/.default"]);
             options.Title = $"{GlobalConfiguration.ApiDocument!.Info.Title} docs | {GlobalConfiguration.ApiDocument.Info.Version}";
         });
     }
