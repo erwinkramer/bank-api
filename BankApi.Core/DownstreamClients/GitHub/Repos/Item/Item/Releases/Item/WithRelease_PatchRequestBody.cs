d4301aec194edeea0effd6e515762316570d4ceb
Erwin Kramer <erwinkramer@hotmail.com>
2026-09-12 16:17:59 +0200

    kiota workspace refresh
    Signed-off-by: Erwin Kramer <erwinkramer@hotmail.com>

--- BankApi.Core/DownstreamClients/GitHub/Repos/Item/Item/Releases/Item/WithRelease_PatchRequestBody.cs
+++ BankApi.Core/DownstreamClients/GitHub/Repos/Item/Item/Releases/Item/WithRelease_PatchRequestBody.cs
@@ -66,6 +66,7 @@ namespace DownstreamClients.GitHub.Repos.Item.Item.Releases.Item
         public WithRelease_PatchRequestBody()
         {
             AdditionalData = new Dictionary<string, object>();
+            MakeLatest = global::DownstreamClients.GitHub.Repos.Item.Item.Releases.Item.WithRelease_PatchRequestBody_make_latest.True;
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
